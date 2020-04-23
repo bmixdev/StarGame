aeed8207883533c60a26d51e38804f76520fd3c1
Mikhail Belin <codxxx@gmail.com>
2020-04-23 20:58:27 +0300

    Инициализация проекта

---
+++ desktop/src/ru/mbelin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.mbelin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.mbelin.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
