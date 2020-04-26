8f0bf056586af5659ae354d0d8005a78fd84e7fd
Mikhail Belin <codxxx@gmail.com>
2020-04-26 19:40:23 +0300

    Первоначальная инициализация

--- core/src/ru/mbelin/StarGame.java
+++ core/src/ru/mbelin/StarGame.java
@@ -4,32 +4,50 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
 	float x, y;
-	
+	private SpriteBatch spriteBatch;
+	private BitmapFont font;
+	int screenWidth, screenHeight;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.jpg");
 		x = y = 0;
+		spriteBatch = new SpriteBatch();
+		font = new BitmapFont();
+	}
+
+	private void drawText() {
+
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+
+		CharSequence str = "Screen (Width: " +screenWidth+"; Height: "+screenHeight+";)";
+		spriteBatch.begin();
+		font.draw(spriteBatch, str, 10, 20);
+		spriteBatch.end();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
+
+		spriteBatch.begin();
+		spriteBatch.draw(img, 0 , 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		spriteBatch.end();
+
+		drawText();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		spriteBatch.dispose();
 		img.dispose();
 	}
 }
