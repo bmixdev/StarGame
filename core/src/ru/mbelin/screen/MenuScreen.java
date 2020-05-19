8eae0113e2f5c1d87f8e1936853253d240c8f0be
Mikhail Belin <codxxx@gmail.com>
2020-05-19 17:09:40 +0300

    enemypool

--- core/src/ru/mbelin/screen/MenuScreen.java
+++ core/src/ru/mbelin/screen/MenuScreen.java
@@ -1,7 +1,10 @@
 package ru.mbelin.screen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
@@ -10,6 +13,7 @@ import ru.mbelin.math.Rect;
 import ru.mbelin.sprite.Background;
 import ru.mbelin.sprite.ButtonExit;
 import ru.mbelin.sprite.ButtonPlay;
+import ru.mbelin.sprite.Logo;
 import ru.mbelin.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
@@ -17,10 +21,18 @@ public class MenuScreen extends BaseScreen {
     private final Game game;
     private Texture bg;
     private Background background;
+
+    private Texture lg;
+    private Logo logo;
+    private Vector2 moveToVector;
+    private BitmapFont font;
+
     private TextureAtlas atlas;
     private ButtonExit buttonExit;
     private ButtonPlay buttonPlay;
     private Star[] stars;
+
+    private Music music;
 
     public MenuScreen(Game game) {
         this.game = game;
@@ -29,6 +41,10 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void show() {
         super.show();
+        font = new BitmapFont();
+        font.setColor(0.4f, 0.5f, 2f, 0.51f);
+        moveToVector = new Vector2(0, 0);
+
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
         atlas = new TextureAtlas("textures/menu/menuAtlas.tpack");
@@ -38,11 +54,18 @@ public class MenuScreen extends BaseScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(atlas);
         }
+        lg = new Texture("textures/stargame.png");
+        logo = new Logo(lg);
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setLooping(true);
+        music.play();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        logo.resize(worldBounds);
         buttonExit.resize(worldBounds);
         buttonPlay.resize(worldBounds);
         for (Star star : stars) {
@@ -59,9 +82,19 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public void dispose() {
+        lg.dispose();
         bg.dispose();
         atlas.dispose();
+        music.dispose();
         super.dispose();
+    }
+
+    private void drawText() {
+        CharSequence str = "Screen (Width: " + Gdx.graphics.getWidth()+"; Height: "+Gdx.graphics.getHeight()+";)";
+        batch.begin();
+        font.draw(batch, str, 10, 20);
+        font.draw(batch, "MoveTo("+moveToVector.x+","+moveToVector.y+")", 10, 40);
+        batch.end();
     }
 
     @Override
@@ -82,6 +115,8 @@ public class MenuScreen extends BaseScreen {
         for (Star star : stars) {
             star.update(delta);
         }
+        buttonPlay.update(delta);
+        buttonExit.update(delta);
     }
 
     private void draw() {
@@ -90,6 +125,7 @@ public class MenuScreen extends BaseScreen {
         for (Star star : stars) {
             star.draw(batch);
         }
+        logo.draw(batch);
         buttonExit.draw(batch);
         buttonPlay.draw(batch);
         batch.end();
